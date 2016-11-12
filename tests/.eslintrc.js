c505c62a71f7d4f1825674b10ef07cda51368db5
Adrian Zalewski <aazalewski@hotmail.com>
2016-11-12 15:16:25 +0100

    Ember-cli-eslint, custom ESLint config - all in place of default JSHint setup.

---
+++ tests/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    embertest: true
+  },
+  rules: {
+    'prefer-const': 0,
+    'no-var': 0,
+    'no-empty': 0,
+    'comma-dangle': 0,
+    'array-callback-return': 0,
+    'object-shorthand': 0,
+    'import/imports-first': 0,
+    'import/no-mutable-exports': 0,
+    'netguru-ember/named-functions-in-promises': 0,
+    'netguru-ember/local-modules': 0,
+  },
+};
